5a590924abaa52b1aef7879cf4f96fd7fa8cb102
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-01 17:09:44 +0300

    betty styles and task 9

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,12 +12,11 @@ size_t tree_height(const binary_tree_t *tree);
 size_t tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
-	
 	left_height = tree_height(tree->left);
 	right_height = tree_height(tree->right);
-	
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
@@ -32,11 +31,10 @@ size_t tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
-	
 	left_height = tree_height(tree->left);
 	right_height = tree_height(tree->right);
-	
 	return (left_height - right_height);
 }
