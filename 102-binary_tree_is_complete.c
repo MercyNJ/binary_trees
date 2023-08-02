a8d376a27a2149b02712aca65f75aea9ede2add8
MercyNJ <mercinjuguna@gmail.com>
2023-08-02 07:00:13 -0700

    A  function that checks if a binary tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,33 +1,49 @@
 #include "binary_trees.h"
 
-int binary_tree_is_complete(const binary_tree_t *tree)
-{
-	const binary_tree_t *queue, *current;
-	int front = 0, rear = 0;
-	int is_last_l = 0;
 
-	if (tree == NULL)
-		return (0);
-	queue = malloc(sizeof(binary_tree_t));
-	if (queue == NULL)
-		return (0);
-	queue[rear++] = tree;
-	while (front < rear)
-	{
-		current = queue[front++];
-		if (current == NULL)
-			is_last_l = 1;
-		else if (is_last_l)
-			return (0);
-		func(current->n);
-		if (current->left != NULL)
-			queue[rear++] = current->left;
-		else
-			is_last_l = 1;
-		if (current->right != NULL)
-			queue[rear++] = current->right;
-		else
-			is_last_l = 1;
-	}
-	return (1);
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is complete, 0 otherwise.
+ */
+int binary_tree_is_complete(const binary_tree_t *tree) {
+    if (tree == NULL) {
+        return 0;
+    }
+
+    int flag = 0;
+    const binary_tree_t **queue = (const binary_tree_t **)malloc(sizeof(const binary_tree_t *));
+    int front = 0, rear = 0;
+
+    queue[rear++] = tree;
+
+    while (front < rear) {
+        const binary_tree_t *node = queue[front++];
+
+        if (node->left) {
+            if (flag) {
+                free(queue);
+                return 0;
+            }
+            queue = (const binary_tree_t **)realloc(queue, (rear + 1) * sizeof(const binary_tree_t *));
+            queue[rear++] = node->left;
+        } else {
+            flag = 1;
+        }
+
+        if (node->right) {
+            if (flag) {
+                free(queue);
+                return 0;
+            }
+            queue = (const binary_tree_t **)realloc(queue, (rear + 1) * sizeof(const binary_tree_t *));
+            queue[rear++] = node->right;
+        } else {
+            flag = 1;
+        }
+    }
+
+    free(queue);
+    return 1;
 }
